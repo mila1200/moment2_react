fd8477174e38945a8875196972c69df9fd0e2df6
Mikael Larsson <s.mikael.larsson@gmail.com>
2025-02-15 18:58:46 +0100

    skriver ut poster i lista

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 //importera states
 import { useState, useEffect } from "react"
+import Todo from "./components/Todo"
 
 interface TodoInterface {
   _id: string,
@@ -11,41 +12,53 @@ interface TodoInterface {
 function App() {
 
   //states för komponenten
-const [todos, setTodos] = useState<TodoInterface | []>([])
-const [loading, setLoading] = useState<boolean>(false)
-const [error, setError] = useState<string | null>(null)
+  const [todos, setTodos] = useState<TodoInterface[] | []>([])
+  const [loading, setLoading] = useState<boolean>(false)
+  const [error, setError] = useState<string | null>(null)
 
-useEffect(() => {
-  fetchData();
-}, [])
+  useEffect(() => {
+    fetchData();
+  }, [])
 
-//hämta data
-const fetchData = async () => {
-  try {
-    setLoading(true);
+  //hämta data
+  const fetchData = async () => {
+    try {
+      setLoading(true);
 
-    const res = await fetch ("http://localhost:5000/todo");
+      const res = await fetch("http://localhost:5000/todo");
 
-    if(!res.ok) {
-      throw Error("Det blev ett fel: " + res.status);
+      if (!res.ok) {
+        throw Error("Det blev ett fel: " + res.status);
+      }
+
+      const data = await res.json();
+      setTodos(data);
+
+
+    } catch (error) {
+      setError("Det blev ett fel vid inhämtning av poster...");
+    } finally {
+      setLoading(false);
     }
-
-    const data = await res.json();
-    setTodos(data);
-
-
-  } catch (error) {
-    setError("Det blev ett fel vid inhämtning av poster...");
-  } finally {
-    setLoading(false);
   }
-}
 
 
   return (
-    <>
+    <main>
       <h1>Att göra</h1>
-    </>
+
+      {loading && <p>Laddar...</p>}
+      {error && <p>{error}</p>}
+
+      <section>
+        {
+          todos.map((todo) => (
+            <Todo todo={todo} key={todo._id} />
+          ))
+        }
+      </section>
+
+    </main>
   )
 }
 
---
+++ src/components/Todo.tsx
@@ -0,0 +1,9 @@
+function Todo({todo} : {todo: any}) {
+  return (
+    <article>
+        <h1>Test</h1>
+    </article>
+  )
+}
+
+export default Todo
